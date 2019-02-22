a444c7ea52c2d5a9a2921c59f0fd05f18d0786ee
Ali Lahrime <alahrime@email.wm.edu>
2019-02-22 15:52:23 -0500

    finished trelloyes server

--- src/app.js
+++ src/app.js
@@ -3,25 +3,16 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+
+const cardRouter = require('./card/card-router')
+const listRouter = require('./list/list-router')
 
 const app = express()
 
 const winston = require('winston')
+const { NODE_ENV } = require('./config')
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
-});
 
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -30,10 +21,24 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      logger.error(`Unauthorized request to path: ${req.path}`);
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+  // move to the next middleware
+  next()
 })
+
+app.use(cardRouter)
+app.use(listRouter)
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response
